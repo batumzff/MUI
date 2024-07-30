aea5c87d21bd2a325511f2187ab742c1718bc2ae
batumzff <batuhanmuzafferoglu@gmail.com>
2024-07-30 17:23:48 +0300

    botton

--- src/components/TypoButtons.jsx
+++ src/components/TypoButtons.jsx
@@ -1,3 +1,6 @@
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 // or
 // import { Typography } from '@mui/material';
@@ -6,42 +9,64 @@ import Typography from "@mui/material/Typography";
 
 const TypoButtons = () => {
   return (
-    <div>
-      <Typography variant="h5" component="h3" color="turquoise" align="center">
-        Deneme
-      </Typography>
-
-      <Typography variant="body1" color="violet" align="center">
-        bu bir p elementidir
-      </Typography>
-
-      <Typography variant="body2" color="#dc143c" align="center">
-        bu bir p elementidir
-      </Typography>
-
-      <Typography
-        variant="subtitle1"
-        align="center"
-        color="fuchsia"
-        
-        sx={{ color: "white", backgroundColor: "tomato", p: 2 , m: 3}}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-end",
+          border: "1px solid red",
+          p: 2,
+        }}
       >
-        bu bir p elementidir
-      </Typography>
+        <Typography
+          variant="h5"
+          component="h3"
+          color="turquoise"
+          align="center"
+        >
+          Deneme
+        </Typography>
 
-      <Typography variant="subtitle2" align="center">
-        bu bir p elementidir
-      </Typography>
-
-      <Typography variant="button" align="center">
-        bu bir p elementidir
-      </Typography>
-      <div>
-        <Typography variant="caption" align="center">
+        <Typography variant="body1" color="violet" align="center">
           bu bir p elementidir
         </Typography>
-      </div>
-    </div>
+
+        <Typography variant="body2" color="#dc143c" align="center">
+          bu bir p elementidir
+        </Typography>
+
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="fuchsia"
+          sx={{ color: "white", backgroundColor: "tomato", p: 2, m: 3 }}
+        >
+          bu bir p elementidir
+        </Typography>
+
+        <Typography variant="subtitle2" align="center">
+          bu bir p elementidir
+        </Typography>
+
+        <Typography variant="button" align="center">
+          bu bir p elementidir
+        </Typography>
+        <div>
+          <Typography variant="caption" align="center">
+            bu bir p elementidir
+          </Typography>
+        </div>
+      </Box>
+      <Stack direction={"row"} mt={4} spacing={2} justifyContent={"center"}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained" color="secondary">Contained</Button>
+        <Button variant="outlined" color="error">Outlined</Button>
+        <Button variant="outlined" color="warning">Outlined</Button>
+        <Button variant="outlined" color="success">Outlined</Button>
+        <Button variant="contained" color="primary">Contained</Button>
+      </Stack>
+    </>
   );
 };
 
